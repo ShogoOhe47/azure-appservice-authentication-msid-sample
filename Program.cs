e0f463c02ea27e93d7f51526143aeca231647b8d
Shogo Ohe <shogo.ohe.47@gmail.com>
2023-11-28 04:24:11 +0900

    create VS project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace azure_appservice_authentication_msid_sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
